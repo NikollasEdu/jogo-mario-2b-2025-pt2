f74983d4272dd3a530605a7373f7f82ae65f8d21
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-07-07 07:39:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -60,10 +60,10 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
         clearInterval(loop);
         gameOverScreen.style.display = "flex";
-   }     else if (pipePosition < 0 && gameStarted) {
-        updateScore();
-        pipe.style.left = '';
-   }
+    } else if (pipePosition < 0 && gameStarted) {
+     updateScore();
+     pipe.style.left = '';
+    }
 },10);
 
 document.addEventListener("keydown", jump);
